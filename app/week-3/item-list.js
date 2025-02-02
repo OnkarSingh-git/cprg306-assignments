68d32bc6963886bb4782320563e8e850524200f8
OnkarSingh-git <Onkar.Singh@edu.sait.ca>
2025-02-01 19:26:57 -0700

    Week 3 Assignment

---
+++ app/week-3/item-list.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Item from "./item";
+
+const items = [
+  { name: "milk, 4 L 🥛", quantity: 1, category: "dairy" },
+  { name: "bread 🍞", quantity: 2, category: "bakery" },
+  { name: "eggs, dozen 🥚", quantity: 2, category: "dairy" },
+  { name: "bananas 🍌", quantity: 6, category: "produce" },
+  { name: "broccoli 🥦", quantity: 3, category: "produce" },
+  { name: "chicken breasts, 1 kg 🍗", quantity: 1, category: "meat" },
+  { name: "pasta sauce 🍝", quantity: 3, category: "canned goods" },
+  { name: "spaghetti, 454 g 🍝", quantity: 2, category: "dry goods" },
+  { name: "toilet paper, 12 pack 🧻", quantity: 1, category: "household" },
+  { name: "paper towels, 6 pack", quantity: 1, category: "household" },
+  { name: "dish soap 🍽️", quantity: 1, category: "household" },
+  { name: "hand soap 🧼", quantity: 4, category: "household" },
+];
+
+const ItemList = () => {
+  return (
+    <ul className="p-2 space-y-4">
+      {items.map((item, index) => (
+        <Item
+          key={index}
+          name={item.name}
+          quantity={item.quantity}
+          category={item.category}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ItemList;
