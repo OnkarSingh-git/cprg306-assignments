46cf1afc7710dddc0e9a8ef043710c25106dec49
OnkarSingh-git <Onkar.Singh@edu.sait.ca>
2025-03-03 21:05:04 -0700

    Week-2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Onkar Singh</p>
+      <Link href="https://github.com/OnkarSingh-git/cprg306-assignments">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
